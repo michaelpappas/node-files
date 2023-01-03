cecd3372b6d362e80aa7e1261bc988c7eb649b53
michaelpappas <pappas.michael@gmail.com>
2023-01-03 11:57:59 -0800

    completed part2)

--- step2.js
+++ step2.js
@@ -1,7 +1,18 @@
 "use strict"
 
 const fsP = require("fs/promises");
+const axios = require("axios")
 
+async function webCat(path){
+  try{
+    const resp = await axios.get(path);
+    console.log(resp.data.slice(0, 20, "..."));
+  }
+  catch(err){
+    console.log("Error:", err)
+    process.exit(1);
+  }
+}
 
 async function cat(path){
   try{
@@ -14,4 +25,10 @@ async function cat(path){
   }
 }
 
-cat(process.argv[2]);
+// cat(process.argv[2]);
+if(process.argv[2].endsWith('txt')){
+  cat(process.argv[2])
+}
+else{
+  webCat(process.argv[2])
+}
